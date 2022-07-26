ef32fe05217825daababa7115a585708d29d2ac5
Jasoncbk998 <jasoncbk998@163.com>
2022-07-26 20:59:47 +0800

    2022-07-26

--- LeetCode/src/main/java/最长公共前缀_14.java
+++ LeetCode/src/main/java/最长公共前缀_14.java
@@ -7,12 +7,15 @@ public class 最长公共前缀_14 {
         if (strs == null || strs.length == 0) {
             return "";
         }
+        // 第一个元素的长度
         int length = strs[0].length();
+        // 元素个数
         int count = strs.length;
         // 外层循环次数
         for (int i = 0; i < length; i++) {
             char c = strs[0].charAt(i);
             for (int j = 1; j < count; j++) {
+                // 第j个元素的第i个字符与第一个元素的第i个字符进行比较
                 if (i == strs[j].length() || strs[j].charAt(i) != c) {
                     return strs[0].substring(0, i);
                 }
--- LeetCode/src/main/java/环形链表_141.java
+++ LeetCode/src/main/java/环形链表_141.java
@@ -2,15 +2,15 @@ import javafx.scene.web.HTMLEditor;
 
 public class 环形链表_141 {
     public static boolean hasCycle(ListNode head) {
-        if(head==null || head.next==null) return false;
-        ListNode slow= head;
-        ListNode fast=head.next;
-        while(slow!=fast){
-            if (fast==null || fast.next==null){
+        if (head == null || head.next == null) return false;
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
                 return false;
             }
-            slow=slow.next;
-            fast=fast.next.next;
+            slow = slow.next;
+            fast = fast.next.next;
         }
         return true;
     }
